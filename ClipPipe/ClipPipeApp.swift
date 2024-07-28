077cbcca5bd2b09585a0ae618eb39bdcb6963156
hiroshi <hiroshi3110@gmail.com>
2024-07-28 19:04:45 +0900

    Create a macOS app Xcode project.

---
+++ ClipPipe/ClipPipeApp.swift
@@ -0,0 +1,17 @@
+//
+//  ClipPipeApp.swift
+//  ClipPipe
+//
+//  Created by hiroshi on 2024/07/28.
+//
+
+import SwiftUI
+
+@main
+struct ClipPipeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
