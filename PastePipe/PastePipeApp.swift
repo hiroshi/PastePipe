8470ce825a4afa18f517c7758d4a847ff568288e
hiroshi <hiroshi3110@gmail.com>
2024-08-15 18:33:57 +0900

    Pipe wasi stdout to a swift pipe instead of writing host process stdout directly.

--- PastePipe/PastePipeApp.swift
+++ PastePipe/PastePipeApp.swift
@@ -1,4 +1,6 @@
 import SwiftUI
+//import System
+import SystemPackage
 import WasmKit
 import WasmKitWASI
 
@@ -9,6 +11,32 @@ struct PastePipeApp: App {
 
     init() {
         print("app init")
+        if let path = Bundle.main.path(forResource: "hello", ofType: "wasm") {
+            print("path: \(path)")
+            do {
+                let data = try Data(contentsOf: URL(fileURLWithPath: path))
+                let module = try parseWasm(bytes: [UInt8](data))
+
+                let (readFD, writeFD) = try FileDescriptor.pipe()
+                let wasi = try WASIBridgeToHost(stdout:writeFD)
+                let runtime = Runtime(hostModules: wasi.hostModules)
+                let instance = try runtime.instantiate(module: module)
+                let exitCode = try wasi.start(instance, runtime: runtime)
+                print("wasm exit code:\(exitCode)")
+                var readBuffer = [UInt8](repeating: 0, count: 1024)
+                let bytesRead = try readBuffer.withUnsafeMutableBytes {
+                            try readFD.read(into: $0)
+                }
+                if bytesRead > 0 {
+                    let output = String(decoding: readBuffer.prefix(bytesRead), as: UTF8.self)
+                    print("Captured output: \(output)")
+                }
+            } catch {
+                print("Error: \(error)")
+            }
+        } else {
+            print("hello.wasm not found.")
+        }
     }
 
     var body: some Scene {
