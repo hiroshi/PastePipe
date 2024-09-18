455ae22b94dd3e5b39efbf96cd8567f4cb7405db
hiroshi <hiroshi3110@gmail.com>
2024-09-18 20:57:57 +0900

    Remove unneeded lines of code.

--- PastePipe/PastePipeApp.swift
+++ PastePipe/PastePipeApp.swift
@@ -2,7 +2,6 @@ import SwiftUI
 
 @main
 struct PastePipeApp: App {
-//    @NSApplicationDelegateAdaptor(AppDelegate.self) var appDelegate
 //    @State private var clipboardText: String = ""
     @StateObject private var pasteboardObserver = PasteboardObserver()
 
@@ -14,12 +13,7 @@ struct PastePipeApp: App {
         // NOTE: onAppear() of views in MenuBarExtra never called, but a StateObject need a view to be installed.
         WindowGroup {
             EmptyView()
-//            .position(x: -1000, y: -1000)
-//            .frame(width: 0, height: 0)
             .onAppear {
-//                if let window = NSApplication.shared.windows.first {
-//                    window.setIsVisible(false)
-//                }
                 pasteboardObserver.startMonitoring { newText in
 //                    clipboardText = newText
                 }
@@ -27,17 +21,7 @@ struct PastePipeApp: App {
             .background(WindowAccessor { window in
                 window.isOpaque = false
                 window.alphaValue = 0
-//                window.isMovable = false
-//                window.orderOut(nil) // ウィンドウを画面から外す
-//                window.titleVisibility = .hidden
-//                window.titlebarAppearsTransparent = true
-//                window.isOpaque = false
-//                window.backgroundColor = NSColor.clear
-//                window.styleMask = [.borderless] // タイトルバーを消す
-//                window.hasShadow = false // 必要に応じて影も削除
-//                window.level = .floating // 他のウィンドウの上に表示する場合は .floating
             })
-
         }
         MenuBarExtra("menu", systemImage: "doc.on.clipboard") {
             if let sourceURL = pasteboardObserver.sourceURL {
@@ -111,34 +95,4 @@ struct WindowReader: NSViewRepresentable {
     }
 
     func updateNSView(_ nsView: NSView, context: Context) {}
-}
-
-
-class AppDelegate: NSObject, NSApplicationDelegate, NSWindowDelegate {
-    func applicationDidFinishLaunching(_ notification: Notification) {
-        // アプリ起動時にカスタマイズ可能
-        if let window = NSApplication.shared.windows.first {
-            window.delegate = self
-            window.alphaValue = 0 // 初期表示を完全に透明にする
-            window.orderOut(nil) // ウィンドウを画面から非表示にする
-        }
-    }
-
-    func applicationWillBecomeActive(_ notification: Notification) {
-        if let window = NSApplication.shared.windows.first {
-            window.delegate = self
-            window.alphaValue = 0 // 初期表示を完全に透明にする
-            window.orderOut(nil) // ウィンドウを画面から非表示にする
-        }
-    }
-//    
-//    // ウィンドウが表示される前にカスタマイズ
-//    func application(_ application: NSApplication, willPresentError error: Error) -> Error {
-//        if let window = NSApplication.shared.windows.first {
-//            window.delegate = self
-//            window.alphaValue = 0 // 初期表示を完全に透明にする
-//            window.orderOut(nil) // ウィンドウを画面から非表示にする
-//        }
-//        return error
-//    }
 }
